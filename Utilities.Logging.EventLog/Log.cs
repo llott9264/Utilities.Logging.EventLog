818b0dae31847f12dade6b48a3d1c293f2f01a65
Lee Lott <llott88@gmail.com>
2024-06-01 18:04:20 -0500

    Created new project for repo. resolves #1.

---
+++ Utilities.Logging.EventLog/Log.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+
+namespace Utilities.Logging.EventLog;
+
+public class Log : ILog
+{
+	private readonly ILogger _logger;
+
+	public Log()
+	{
+		_logger = LogManager.GetCurrentClassLogger();
+	}
+
+	public void Information(string message)
+	{
+		_logger.Info(message);
+	}
+
+	public void Warning(string message)
+	{
+		_logger.Warn(message);
+	}
+
+	public void Debug(string message)
+	{
+		_logger.Debug(message);
+	}
+
+	public void Error(string message)
+	{
+		_logger.Error(message);
+	}
+}
