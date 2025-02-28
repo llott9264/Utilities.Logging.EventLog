9254cd402055b1897ef0d472be578a5c5de52065
Lee Lott <llott88@gmail.com>
2025-02-27 18:46:27 -0600

    #9 - Added MediatR Support.

---
+++ Utilities.Logging.EventLog/MediatR/CreateLogCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using NLog;
+
+namespace Utilities.Logging.EventLog.MediatR;
+
+public class CreateLogCommandHandler(ILog logger) : IRequestHandler<CreateLogCommand>
+{
+	public Task Handle(CreateLogCommand request, CancellationToken cancellationToken)
+	{
+		switch (request.LogType)
+		{
+			case LogType.Debug:
+				logger.Debug(request.Message);
+				break;
+			case LogType.Information:
+				logger.Information(request.Message);
+				break;
+			case LogType.Warning:
+				logger.Warning(request.Message);
+				break;
+			case LogType.Error:
+				logger.Error(request.Message);
+				break;
+			default:
+				string message = $"Log Type {request.LogType} not found.  Could not write message to Log:  {request.Message}";
+				logger.Error(message);
+				break;
+		}
+
+		return Task.CompletedTask;
+	}
+}
